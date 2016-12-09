f49f94b5dd12b3ae4dbe23de6f47338533b69f5d
Marek Felsoci <marek.felsoci@gmail.com>
2016-12-09 22:12:23 +0100

    Début de la création des classes

---
+++ controllers/Controller_Category.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Marek
+ * Date: 9.12.2016
+ * Time: 22:10
+ */
+class Controller_Category
+{
+
+}
---
+++ models/Kernel.php
@@ -0,0 +1,29 @@
+<?php
+
+abstract class Kernel
+{
+    private static $connection;
+
+    public function __construct($array)
+    {
+        foreach($array as $index => $value)
+        {
+            $item_name = ucfirst($index);
+            $method = 'set'.$item_name;
+            if(method_exists($this, $method))
+            {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public static function setConnection(PDO $connection)
+    {
+        self::$connection = $connection;
+    }
+
+    public static function getConnection()
+    {
+        return self::$connection;
+    }
+}
